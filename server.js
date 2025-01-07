c2802e53228eafbd074992765bae1d0bef009739
Dhruv Krishna Vaid <69188774+dhruvkrishnavaid@users.noreply.github.com>
2025-01-07 07:22:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ env.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
